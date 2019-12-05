09feeaf97c3e2b162d5136822e721a4d11782f77
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-05 16:02:47 -0500

    changes to sra dump for Yuan 2016

--- bash/sradump.sh
+++ bash/sradump.sh
@@ -10,7 +10,8 @@ mkdir ~/computing/sequences/srareads/#projectname/fastqs
 #Change directory to sratoolkit for access to programs for sequence acquisition
 cd ~/computing/programs/sratoolkit.2.8.2-1-mac64/bin
 #Run SRA acquisition command by specifying variable containing sequence file prefixes
-SRRnums="SRR#######"
+#Yuan et al. 2016 only
+SRRnums="SRR3727281 SRR3727374 SRR3727379 SRR3727380 SRR3727383 SRR3727387 SRR3727388 SRR3727390 SRR3727400 SRR3727418 SRR3727424 SRR3727425"
 #Download from NCBI and convert SRA files into .fastq format in output directories
 #fastq-dump [options] <accession>
 #For loop to automatic .fastq conversion
@@ -20,7 +21,7 @@ echo "This is the SRA file being downloaded and converted to .fastq format"
 echo $SRRnum
 ./fastq-dump --split-files \
 --outdir ~/computing/sequences/srareads/#projectname/fastqs \
-${SRRnum} 
+${SRRnum}
 echo "$SRRnum acquired and converted to .fastq format"
 done
 
